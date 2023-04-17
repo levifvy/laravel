03d1a354ddd259f392f1adb35a425f9b2882e209
levifvy <levifv.madrid@gmail.com>
2023-04-17 02:23:10 +0200

    iniciando con  estilos simples

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,3 +1,12 @@
-<footer>
-    <h3>Here goes the footer</h3>
+<footer class="bg-gradient-to-r from-blue-500 to-purple-500 py-8">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="border-t border-b border-gray-800 py-4">
+            <div class="flex flex-wrap justify-center">
+              <a href="#" class="tab rounded-l-lg bg-white text-gray-800 font-medium px-4 py-2 border border-gray-800 hover:bg-gray-800 hover:text-white">Phone</a>
+              <a href="#" class="tab bg-white text-gray-800 font-medium px-4 py-2 border border-gray-800 hover:bg-gray-800 hover:text-white">Address</a>
+              <a href="#" class="tab bg-white text-gray-800 font-medium px-4 py-2 border border-gray-800 hover:bg-gray-800 hover:text-white">Follow us</a>
+              <a href="#" class="tab rounded-r-lg bg-white text-gray-800 font-medium px-4 py-2 border border-gray-800 hover:bg-gray-800 hover:text-white">Social Media</a>
+            </div>
+        </div>
+    </div>
 </footer>
--- resources/views/teams/edit.blade.php
+++ resources/views/teams/edit.blade.php
@@ -5,44 +5,61 @@
 @section('content')
     <h1>On this page you can edit a Team</h1>
 
-    <form action="{{route('teams.update', $team)}}" method="post">
+    <form action="{{route('teams.update', $team)}}" method="POST" class="w-full max-w-lg">
 
         @csrf
 
         @method('put')
 
-        <label>
-            Team Name:<br>
-            <input type="text" name="name" value="{{old('name', $team->name)}}">
-        </label>
-
-        @error('name')
-            <small>*{{$message}}</small>
-        @enderror
-
-        <input type="hidden" name="slug" value="slug">
-
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">Team Name:</label>
+                <br>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" name="name" value="{{old('name', $team->name)}}" placeholder="Please fill out this field">
+                @error('name')
+                    <br>
+                    <small>*{{$message}}</small>
+                    <br>
+                @enderror
+            </div>
+            <br>
+            <div class="w-full md:w-1/2 px-3">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">Description:</label>
+                <br>
+                <textarea class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="description" rows="5" placeholder="Tell us about your team.">{{old('description', $team->description)}}</textarea>
+                @error('description')
+                    <br>
+                    <small>*{{$message}}</small>
+                    <br>
+                @enderror
+            </div>
+        </div>
         <br>
-        <label>
-            Description:<br>
-            <textarea name="description" rows="5">{{old('description', $team->description)}}</textarea>
-        </label>
-
-        @error('description')
-            <small>*{{$message}}</small>
-        @enderror
-
-        <br>
-        <label>
-            Category:<br>
-            <input type="text" name="category" value="{{old('category', $team->category)}}">
-        </label>
-
-        @error('category')
-            <small>*{{$message}}</small>
-        @enderror
-
-        <br>
-        <button type="submit">Update this Team</button>
+        <div class="flex flex-wrap -mx-3 mb-2">
+            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">Category:</label>
+                <div class="relative">
+                    <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" name="category" value="{{old('category', $team->category)}}">
+                        <option value="" disabled selected>Select one option</option>
+                        <option>First division</option>
+                        <option>Second division</option>
+                        <option>Third division</option>
+                        <option>Amateur</option>
+                    </select>
+                    @error('category')
+                        <br>
+                        <small>*{{$message}}</small>
+                        <br>
+                    @enderror
+                </div>
+                <br>
+                <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" type="submit">
+                        Update
+                    </button>
+                </div>
+            </div>
+        </div>
     </form>
+
 @endsection
